25492df8372289ea7444eba799de8472bc293fac
RiskaSintiaDewi <42725266+RiskaSintiaDewi@users.noreply.github.com>
2018-09-10 05:05:38 -0700

    Selesai

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,43 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$c = 0;
+rekursif2(5);
+
+function rekursif3($d, $n){
+  global $c;
+    if ($d > $c - $n){
+        echo "*";
+        rekursif3($d - 1, $n);
+    }
 }
-rekursif(5);
+function rekursif2($n) {
+    global $c;
+    rekursif3($c, $n);
+    echo "<br>";
+    if (--$n > $c) {
+       rekursif2($n);
+    }
+}
+
+/*================================================*/
+
+echo "<br>";
+
+$nilai=5;
+
+function rekursif($a, $b, $nilai){
+    if ($a <= $nilai){
+        if ($b <= $a){
+        echo $b;
+        $b++;
+        rekursif($a, $b, $nilai);   
+    }else{
+        echo "<br>";
+        $a++;
+        rekursif($a,1,$nilai);
+    }
+}
+}
+rekursif(1, 1, $nilai);
+
 ?>
